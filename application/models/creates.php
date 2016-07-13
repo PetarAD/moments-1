9cf7c7b3fa33d8e892086ae63686814d3be55512
excellent6 <excellent6@abv.bg>
2016-07-13 15:20:14 +0300

    Committed my state

--- application/models/creates.php
+++ application/models/creates.php
@@ -7,7 +7,7 @@ class Creates extends CI_Model
   function add($description, $name)
   {
     $publish=date('y-m-d');
-    $this->db->query("INSERT INTO `event`( `category`, `description`,`name`,`published`) VALUES ('category','$description','$name', '$publish')");
+    $this->db->query("INSERT INTO `moment`( `category`, `description`,`name`,`published`) VALUES ('category','$description','$name', '$publish')");
     return true;
   }
 }
--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -3,10 +3,9 @@
     <ul>
       <li><a href="<?= site_url('index.php') ?>"><i class="fa fa-home" aria-hidden="true"></i>
   Начало</a></li>
-      <li><a href="<?= site_url('category') ?>"><i class="fa fa-filter" aria-hidden="true"></i>
-  Категории</a></li>
+
       <li><a href="<?= site_url('create') ?>"><i class="fa fa-calendar" aria-hidden="true"></i>
-  Създай събитие</a></li>
+  Сподели вълнение</a></li>
       <li><a href="<?= site_url('admin') ?>"><i class="fa fa-lock" aria-hidden="true"></i>
   Админ</a></li>
     </ul>
