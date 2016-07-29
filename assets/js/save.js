b287594dcc85ec2c83070872b9953d012711fb15
excellent6 <excellen6@abv.bg>
2016-07-29 20:51:25 +0300

    Крайни промени

--- assets/js/save.js
+++ assets/js/save.js
@@ -2,6 +2,6 @@ var saveButton=document.getElementById('save');
 saveButton.addEventListener('click', saveImage );
 function saveImage(){
   var data =canvas.toDataURL();
-//document.getElementById('canvasImg').src= data;
-window.open(data,'_blank', 'location=0, menubar=0;');
+document.getElementById('canvasImg').alt = 'Съхрани снимката';
+document.getElementById('canvasImg').src= data;
 }
