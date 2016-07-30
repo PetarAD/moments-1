5e28ec1e7260f56a53b0479bead1b3eda04ec849
PetarAD <petar.ad@mail.bg>
2016-07-30 14:37:41 +0300

    gal
    gal

---
+++ application/views/gallery.php
@@ -0,0 +1,5 @@
+<form action="<?= site_url('/gallery/add') ?>" method="post">
+  <input type="text" name="name" placeholder="Name">
+  <input type="file" name="img" >
+  <input type="submit" name="submit" value="Създай">
+</form>
