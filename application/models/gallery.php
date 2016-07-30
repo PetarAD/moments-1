fe2379d7ed59420b1030391e4ce805490a81f4ef
PetarAD <petar.ad@mail.bg>
2016-07-30 15:28:17 +0300

    some
    some

--- application/models/gallery.php
+++ application/models/gallery.php
@@ -4,7 +4,7 @@ class Gallery extends CI_Model
   function add( $name,$img)
   {
     $img = file_get_contents($img['fill_path']);
-    $this->db->query("INSERT INTO `img`( `name`, `img`) VALUES (?, ?, ?, ?)", [$name, $img]);
+    $this->db->query("INSERT INTO `img`( `name`, `img`) VALUES ( ?, ?)", [$name, $img]);
     return true;
   }
 }
