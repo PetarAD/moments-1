2b606e7e9549551e69b1c1c4cd88c5df0bb92044
excellent6 <excellen6@abv.bg>
2016-07-30 15:51:05 +0300

    Живи и здрави винаги лято

--- application/models/gallery.php
+++ application/models/gallery.php
@@ -3,8 +3,8 @@ class Gallery extends CI_Model
 {
   function add( $name,$img)
   {
-    $img = file_get_contents($img['fill_path']);
-    $this->db->query("INSERT INTO `img`( `name`, `img`) VALUES (?, ?, ?, ?)", [$name, $img]);
+    $img = file_get_contents($img);
+    $this->db->query("INSERT INTO `img`( `name`, `img`) VALUES (?, ?)", [$name, $img]);
     return true;
   }
 }
--- application/views/gallery.php
+++ application/views/gallery.php
@@ -1,5 +1,5 @@
-<form action="<?= site_url('/galleries/add') ?>" method="post">
-  <input type="text" name="name" placeholder="Name">
+<form action="<?= site_url('/galleries/add') ?>" enctype="multipart/formmethod="post">
+  <input type="text" name="name" placeholder="Име">
   <input type="file" name="img" >
   <input type="submit" name="submit" value="Създай">
 </form>
